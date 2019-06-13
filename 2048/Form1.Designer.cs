4533d179c9c17e642f40aefe6ad2a289d2343408
nguyenngocthi02011999 <51789419+nguyenngocthi02011999@users.noreply.github.com>
2019-06-13 20:45:44 +0700

    first commit

---
+++ 2048/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace _2048
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_kl = new System.Windows.Forms.Label();
+            this.lb_diem = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "F1 để chơi lại";
+            // 
+            // lb_kl
+            // 
+            this.lb_kl.BackColor = System.Drawing.Color.SandyBrown;
+            this.lb_kl.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_kl.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lb_kl.Location = new System.Drawing.Point(522, 73);
+            this.lb_kl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_kl.Name = "lb_kl";
+            this.lb_kl.Size = new System.Drawing.Size(93, 47);
+            this.lb_kl.TabIndex = 14;
+            this.lb_kl.Text = "0";
+            this.lb_kl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb_diem
+            // 
+            this.lb_diem.BackColor = System.Drawing.Color.RosyBrown;
+            this.lb_diem.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_diem.ForeColor = System.Drawing.Color.Snow;
+            this.lb_diem.Location = new System.Drawing.Point(522, 16);
+            this.lb_diem.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_diem.Name = "lb_diem";
+            this.lb_diem.Size = new System.Drawing.Size(93, 47);
+            this.lb_diem.TabIndex = 15;
+            this.lb_diem.Text = "0";
+            this.lb_diem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(289, 81);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(225, 32);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Điểm Cao Nhất:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Algerian", 40.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label2.Location = new System.Drawing.Point(42, 45);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 75);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "2048";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(424, 31);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 32);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Điểm:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(13, 137);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(567, 523);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 673);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb_kl);
+            this.Controls.Add(this.lb_diem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_kl;
+        private System.Windows.Forms.Label lb_diem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+
+    }
+}
+
---
+++ 2048/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace _2048
+{
+    public partial class Form1 : Form
+    {
+        private int KhoangCach = 5;
+
+        private Random rand = new Random();
+        StreamReader doc = new StreamReader("diem2048.txt");
+
+        private int Diem = 0;
+        private Label[,] OSo = new Label[4, 4];
+        int[,] SoO = new int[4, 4];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void frm_laod()
+        {
+            lb_diem.Text = "0";
+            lb_kl.Text = doc.ReadToEnd();
+            doc.Close();
+            if (lb_kl.Text == "")
+                lb_kl.Text = "0";
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    OSo[i, j] = new Label();
+                    OSo[i, j].Location = new Point(KhoangCach + i * (100 + KhoangCach), KhoangCach + j * (100 + KhoangCach));
+                    OSo[i, j].Size = new Size(100, 100);
+                    OSo[i, j].TabIndex = i * 4 + j;
+                    OSo[i, j].Name = String.Format("lb%d%d", i, j);
+                    OSo[i, j].BackColor = Color.FromName("ActiveBorder");
+                    OSo[i, j].Font = new Font("Consolas", 18F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+                    OSo[i, j].TextAlign = ContentAlignment.MiddleCenter;
+                    groupBox1.Controls.Add(OSo[i, j]);
+                }
+            }
+            LoadO();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            frm_laod();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (SoO[i, j] == 0)
+                        OSo[i, j].Text = "";
+                    else
+                        OSo[i, j].Text = SoO[i, j].ToString();
+                    setMauChoO(i, j);
+                }
+            }
+            lb_diem.Text = Diem.ToString();
+        }
+
+        bool RanDomViTriHien()
+        {
+            bool isDo = false;
+            List<int> test = new List<int>();
+            for (int i = 0; i < 16; i++)
+            {
+                if (SoO[i / 4, i % 4] == 0)
+                {
+                    test.Add(i);
+                    isDo = true;
+                }
+            }
+            if (test.Count > 0)
+            {
+                int set = test[rand.Next(0, test.Count - 1)];
+                while (SoO[set / 4, set % 4] != 0 && test.Count > 1)
+                {
+                    test.Remove(set);
+                    set = test[rand.Next(0, test.Count - 1)];
+                }
+                SoO[set / 4, set % 4] = rand.Next(1, 100) > 90 ? 4 : 2;
+                Diem += SoO[set / 4, set % 4];
+            }
+            return isDo;
+        }
+
+        bool PhimLen()
+        {
+            bool isDo = false;
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    for (int y1 = y + 1; y1 < 4; y1++)
+                    {
+                        if (SoO[x, y1] > 0)
+                        {
+                            if (SoO[x, y] == 0)
+                            {
+                                SoO[x, y] = SoO[x, y1];
+                                SoO[x, y1] = 0;
+                                y--;
+                                isDo = true;
+                            }
+                            else if (SoO[x, y] == SoO[x, y1])
+                            {
+                                SoO[x, y] *= 2;
+                                SoO[x, y1] = 0;
+                                isDo = true;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            if (isDo)
+                RanDomViTriHien();
+            return isDo;
+        }
+
+        bool PhimXuong()
+        {
+            bool isDo = false;
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 3; y >= 1; y--)
+                {
+                    for (int y1 = y - 1; y1 >= 0; y1--)
+                    {
+                        if (SoO[x, y1] > 0)
+                        {
+                            if (SoO[x, y] == 0)
+                            {
+                                SoO[x, y] = SoO[x, y1];
+                                SoO[x, y1] = 0;
+                                y++;
+                                isDo = true;
+                            }
+                            else if (SoO[x, y] == SoO[x, y1])
+                            {
+                                SoO[x, y] *= 2;
+                                SoO[x, y1] = 0;
+                                isDo = true;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            if (isDo)
+                RanDomViTriHien();
+            return isDo;
+        }
+
+        bool PhimPhai()
+        {
+            bool isDo = false;
+            for (int y = 0; y < 4; y++)
+            {
+                for (int x = 3; x >= 1; x--)
+                {
+                    for (int x1 = x - 1; x1 >= 0; x1--)
+                    {
+                        if (SoO[x1, y] > 0)
+                        {
+                            if (SoO[x, y] == 0)
+                            {
+                                SoO[x, y] = SoO[x1, y];
+                                SoO[x1, y] = 0;
+                                x++;
+                                isDo = true;
+                            }
+                            else if (SoO[x, y] == SoO[x1, y])
+                            {
+                                SoO[x, y] *= 2;
+                                SoO[x1, y] = 0;
+                                isDo = true;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            if (isDo)
+                RanDomViTriHien();
+            return isDo;
+        }
+
+        bool PhimTrai()
+        {
+            bool isDo = false;
+            for (int y = 0; y < 4; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    for (int x1 = x + 1; x1 < 4; x1++)
+                    {
+                        if (SoO[x1, y] > 0)
+                        {
+                            if (SoO[x, y] == 0)
+                            {
+                                SoO[x, y] = SoO[x1, y];
+                                SoO[x1, y] = 0;
+                                x--;
+                                isDo = true;
+                            }
+                            else if (SoO[x, y] == SoO[x1, y])
+                            {
+                                SoO[x, y] *= 2;
+                                SoO[x1, y] = 0;
+                                isDo = true;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            if (isDo)
+                RanDomViTriHien();
+            return isDo;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Up)
+                PhimLen();
+            if (e.KeyData == Keys.Down)
+                PhimXuong();
+            if (e.KeyData == Keys.Right)
+                PhimPhai();
+            if (e.KeyData == Keys.Left)
+                PhimTrai();
+            if (e.KeyData == Keys.F1)
+            {
+                if (Convert.ToInt32(lb_diem.Text) > Convert.ToInt32(lb_kl.Text))
+                {
+                    doc.Close();
+                    StreamWriter ghi = new StreamWriter("diem2048.txt");
+                    ghi.WriteLine(lb_diem.Text);
+                    ghi.Flush();
+                    ghi.Close();
+                    this.Hide();
+                    Form1 fr = new Form1();
+                    fr.Show();
+                }
+                else
+                {
+                    this.Hide();
+                    Form1 fr = new Form1();
+                    fr.Show();
+                }
+            }
+            this.Refresh();
+            if (CheckGameOver())
+            {
+                if (Convert.ToInt32(lb_diem.Text) > Convert.ToInt32(lb_kl.Text))
+                {
+                    doc.Close();
+                    StreamWriter ghi = new StreamWriter("diem2048.txt");
+                    ghi.WriteLine(lb_diem.Text);
+                    ghi.Flush();
+                    ghi.Close();
+                }
+                if (MessageBox.Show("Điểm: " + Diem.ToString() + "\n" + " chơi lại?",
+                    "Game Over!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                    Application.Exit();
+                else
+                {
+                    this.Hide();
+                    Form1 fr = new Form1();
+                    fr.Show();
+                }
+            }
+        }
+        bool CheckGameOver()
+        {
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    if (SoO[x, y] == 0 ||
+                        (y < 3 && SoO[x, y] == SoO[x, y + 1]) ||
+                        (x < 3 && SoO[x, y] == SoO[x + 1, y]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        void LoadO()
+        {
+            Diem = 0;
+            for (int x = 0; x < 4; x++)
+                for (int y = 0; y < 4; y++)
+                    SoO[x, y] = 0;
+            RanDomViTriHien();
+            RanDomViTriHien();
+            this.Refresh();
+        }
+
+        void setMauChoO(int x, int y)
+        {
+            switch (SoO[x, y])
+            {
+                case 0: OSo[x, y].BackColor = Color.FromArgb(220, 220, 220); break;
+                case 2: OSo[x, y].BackColor = Color.FromArgb(255, 192, 192); break;
+                case 4: OSo[x, y].BackColor = Color.FromArgb(255, 128, 128); break;
+                case 8: OSo[x, y].BackColor = Color.FromArgb(255, 224, 192); break;
+                case 16: OSo[x, y].BackColor = Color.FromArgb(255, 192, 128); break;
+                case 32: OSo[x, y].BackColor = Color.FromArgb(255, 255, 192); break;
+                case 64: OSo[x, y].BackColor = Color.FromArgb(255, 255, 128); break;
+                case 128: OSo[x, y].BackColor = Color.FromArgb(192, 255, 192); break;
+                case 256: OSo[x, y].BackColor = Color.FromArgb(128, 255, 128); break;
+                case 512: OSo[x, y].BackColor = Color.FromArgb(192, 255, 255); break;
+                case 1024: OSo[x, y].BackColor = Color.FromArgb(128, 255, 255); break;
+                case 2048: OSo[x, y].BackColor = Color.FromArgb(192, 192, 255); break;
+                case 4096: OSo[x, y].BackColor = Color.FromArgb(128, 128, 255); break;
+                case 8192: OSo[x, y].BackColor = Color.FromArgb(255, 192, 255); break;
+            }
+        }
+
+
+
+    }
+}
